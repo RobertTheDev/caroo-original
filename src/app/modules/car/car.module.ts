b541e7bf1a4bb720d7132b27ec108f3bf597419c
RobertTheDev <robertthedev@gmail.com>
2023-10-02 00:57:03 +0100

    feat: declared car views and components

--- src/app/modules/car/car.module.ts
+++ src/app/modules/car/car.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CarCardComponent } from './components/car-card/car-card.component';
+import { CreateCarFormComponent } from './components/create-car-form/create-car-form.component';
+import { UpdateCarFormComponent } from './components/update-car-form/update-car-form.component';
+import { CarsViewComponent } from './views/cars-view/cars-view.component';
+import { CreateCarViewComponent } from './views/create-car-view/create-car-view.component';
+import { CarViewComponent } from './views/car-view/car-view.component';
+import { UpdateCarViewComponent } from './views/update-car-view/update-car-view.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    CarCardComponent,
+    CreateCarFormComponent,
+    UpdateCarFormComponent,
+    CarsViewComponent,
+    CreateCarViewComponent,
+    CarViewComponent,
+    UpdateCarViewComponent,
+  ],
+  imports: [CommonModule],
 })
-export class CarModule { }
+export class CarModule {}

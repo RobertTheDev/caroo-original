ba1a54ab8c773b6b3026d0575de395a9efa6e313
RobertTheDev <robertthedev@gmail.com>
2023-10-02 00:17:38 +0100

    feat: added winston logger to log the error

--- api/modules/car/controllers/createCar.ts
+++ api/modules/car/controllers/createCar.ts
@@ -1,34 +1,35 @@
 import prisma from 'api/lib/db/prisma';
+import logger from 'api/lib/logger/winstonLogger';
 import { Request, Response } from 'express';
 import createCarSchema from 'models/car/validators/createCar.schema';
 
-// This handler creates a car in the database.
+// This handler validates the request body and if successful creates a car in the database.
 
 export default async function createCar(req: Request, res: Response) {
   try {
-    // Get the body from the request.
+    // STEP 1: Validate the request body using the create car schema.
     const { body } = req;
-
-    //  Validate the body using the create car schema.
-    const validation = await createCarSchema.safeParseAsync(body);
+    const validatedBody = await createCarSchema.safeParseAsync(body);
 
     // If validation is unsuccessful return an error.
-    if (!validation.success) {
+    if (!validatedBody.success) {
       return res.status(400).json({
         success: false,
-        message: validation.error.errors[0].message,
+        message: validatedBody.error.errors[0].message,
       });
     }
 
-    // Create a car in the database.
-    const car = await prisma.car.create({
-      data: validation.data,
+    // STEP 2: If validation is successful create and return the created car.
+    const createdCar = await prisma.car.create({
+      data: validatedBody.data,
     });
-
-    // Return the car.
-    return res.status(200).json({ success: true, data: car });
+    return res.status(201).json({ success: true, data: createdCar });
   } catch (error) {
-    // Return an error if one occurs.
+    // If an error occurs then log the error and return an unsuccessful statement.
+    const errorMessage = (error as Error).message;
+    logger.error(
+      `Error in route ${req.method} ${req.originalUrl}: ${errorMessage}`
+    );
     return res.status(400).json({ success: false });
   }
 }

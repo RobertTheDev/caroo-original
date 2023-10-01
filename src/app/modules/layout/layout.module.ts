3b3391d48d64816e048bf4cfb31f665d3bed7680
RobertTheDev <robertthedev@gmail.com>
2023-10-02 00:48:20 +0100

    feat: declares layout components

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    PageLayoutComponent
+  ],
   imports: [
     CommonModule
   ]

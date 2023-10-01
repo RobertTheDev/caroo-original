234c212658335ea6d5d70749833e21e3296b2ace
RobertTheDev <robertthedev@gmail.com>
2023-10-01 23:55:04 +0100

    feat: defined car colour values

---
+++ models/car/values/carColourValues.ts
@@ -0,0 +1,21 @@
+// Defines the possible values for a car colur.
+
+const carColourValues = [
+  'Beige',
+  'Black',
+  'Blue',
+  'Brown',
+  'Cream',
+  'Gold',
+  'Green',
+  'Grey',
+  'Orange',
+  'Pink',
+  'Purple',
+  'Red',
+  'Silver',
+  'White',
+  'Yellow',
+];
+
+export default carColourValues;

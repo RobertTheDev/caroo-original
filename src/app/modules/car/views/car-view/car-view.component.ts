5de60e3e1836f65b0d7007da734a8218aef9d081
RobertTheDev <robertthedev@gmail.com>
2023-10-02 00:56:06 +0100

    feat: created car view component

---
+++ src/app/modules/car/views/car-view/car-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-view',
+  templateUrl: './car-view.component.html',
+  styleUrls: ['./car-view.component.scss']
+})
+export class CarViewComponent {
+
+}

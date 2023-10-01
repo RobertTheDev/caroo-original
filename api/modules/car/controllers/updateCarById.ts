d8e8699d0b5e2be5f7e1f35e0160596011989d1b
RobertTheDev <robertthedev@gmail.com>
2023-10-01 23:54:54 +0100

    feat: added update car validation schema

--- api/modules/car/controllers/updateCarById.ts
+++ api/modules/car/controllers/updateCarById.ts
@@ -1,5 +1,6 @@
 import prisma from 'api/lib/db/prisma';
 import { Request, Response } from 'express';
+import updateCarSchema from 'models/car/validators/updateCar.schema';
 
 // This handler updates a car using its unique id in the database.
 
@@ -10,6 +11,17 @@ export default async function updateCarById(req: Request, res: Response) {
 
     // Get id from the request params.
     const { id } = params;
+
+    //  Validate the body using the update car schema.
+    const validation = await updateCarSchema.safeParseAsync(body);
+
+    // If validation is unsuccessful return an error.
+    if (!validation.success) {
+      return res.status(400).json({
+        success: false,
+        message: validation.error.errors[0].message,
+      });
+    }
 
     // Update the car.
     const updatedCar = await prisma.car.update({

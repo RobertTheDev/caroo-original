4d07219bdc7570a1a3a33c229a0ab727e2cdc799
RobertTheDev <robertthedev@gmail.com>
2023-10-02 00:56:19 +0100

    feat: created update car form component

---
+++ src/app/modules/car/components/update-car-form/update-car-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpdateCarFormComponent } from './update-car-form.component';
+
+describe('UpdateCarFormComponent', () => {
+  let component: UpdateCarFormComponent;
+  let fixture: ComponentFixture<UpdateCarFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UpdateCarFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UpdateCarFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/car/components/update-car-form/update-car-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-car-form',
+  templateUrl: './update-car-form.component.html',
+  styleUrls: ['./update-car-form.component.scss']
+})
+export class UpdateCarFormComponent {
+
+}

8393d2c8a5eba83d1a359e966d1b4c890198b6fb
RobertTheDev <robertthedev@gmail.com>
2023-10-01 23:54:30 +0100

    feat: added create car validation schema

--- api/modules/car/controllers/createCar.ts
+++ api/modules/car/controllers/createCar.ts
@@ -1,5 +1,6 @@
 import prisma from 'api/lib/db/prisma';
 import { Request, Response } from 'express';
+import createCarSchema from 'models/car/validators/createCar.schema';
 
 // This handler creates a car in the database.
 
@@ -8,15 +9,26 @@ export default async function createCar(req: Request, res: Response) {
     // Get the body from the request.
     const { body } = req;
 
+    //  Validate the body using the create car schema.
+    const validation = await createCarSchema.safeParseAsync(body);
+
+    // If validation is unsuccessful return an error.
+    if (!validation.success) {
+      return res.status(400).json({
+        success: false,
+        message: validation.error.errors[0].message,
+      });
+    }
+
     // Create a car in the database.
     const car = await prisma.car.create({
-      data: body,
+      data: validation.data,
     });
 
     // Return the car.
-    res.status(200).json({ success: true, data: car });
+    return res.status(200).json({ success: true, data: car });
   } catch (error) {
     // Return an error if one occurs.
-    res.status(400).json({ success: false });
+    return res.status(400).json({ success: false });
   }
 }

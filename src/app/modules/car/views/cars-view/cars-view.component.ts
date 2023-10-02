733ee80c516fb7fb3aeaafbd26668cf6f903331d
RobertTheDev <robertthedev@gmail.com>
2023-10-02 01:35:39 +0100

    feat: fetched data using get cars rxjs handler

--- src/app/modules/car/views/cars-view/cars-view.component.ts
+++ src/app/modules/car/views/cars-view/cars-view.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import ICar from 'models/car/interfaces/Car';
+import { CarService } from 'src/app/services/car/car.service';
 
 @Component({
   selector: 'app-cars-view',
   templateUrl: './cars-view.component.html',
-  styleUrls: ['./cars-view.component.scss']
+  styleUrls: ['./cars-view.component.scss'],
 })
-export class CarsViewComponent {
+export class CarsViewComponent implements OnInit {
+  constructor(private carService: CarService) {}
 
+  cars: ICar[] = [];
+  loading = true;
+  errorMessage: string | null = null;
+
+  ngOnInit() {
+    this.carService.getCars().subscribe({
+      next: (data) => {
+        this.loading = false;
+        this.cars = data.data;
+      },
+      error: (error) => {
+        this.loading = false;
+        this.errorMessage = error;
+      },
+    });
+  }
 }

a7bb8713576fc47df3c48b9f9abf612fe4bc25a6
RobertTheDev <robertthedev@gmail.com>
2023-10-02 01:35:20 +0100

    feat: defined get cars rxjs handler

--- src/app/services/car/car.service.ts
+++ src/app/services/car/car.service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import ICar from 'models/car/interfaces/Car';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CarService {
-  constructor() {}
+  private options = { withCredentials: true };
+
+  constructor(private http: HttpClient) {}
+
+  getCars(): Observable<{ data: ICar[] }> {
+    return this.http.get<{ data: ICar[] }>('http://localhost:4200/api/cars');
+  }
 }

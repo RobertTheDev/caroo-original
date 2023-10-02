8941231d3830ad9ca64136426bea0b4811571d2c
RobertTheDev <robertthedev@gmail.com>
2023-10-02 01:52:00 +0100

    feat: added get car rxjs handler

--- src/app/modules/car/views/car-view/car-view.component.ts
+++ src/app/modules/car/views/car-view/car-view.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import ICar from 'models/car/interfaces/Car';
+import { CarService } from 'src/app/services/car/car.service';
 
 @Component({
   selector: 'app-car-view',
   templateUrl: './car-view.component.html',
-  styleUrls: ['./car-view.component.scss']
+  styleUrls: ['./car-view.component.scss'],
 })
-export class CarViewComponent {
+export class CarViewComponent implements OnInit {
+  constructor(private carService: CarService, private route: ActivatedRoute) {}
 
+  car: ICar | null = null;
+
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+
+    this.carService.getCarById(id).subscribe({
+      next: (data) => {
+        this.car = data.data;
+      },
+      error: (error) => {
+        console.log(error.error);
+      },
+    });
+  }
 }

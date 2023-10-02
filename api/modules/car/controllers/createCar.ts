d1e4f6ab5a44e19864e0a03aab5f4d86aec3ce1c
RobertTheDev <robertthedev@gmail.com>
2023-10-02 02:13:16 +0100

    feat: added winston logger and update commenting

--- api/modules/car/controllers/createCar.ts
+++ api/modules/car/controllers/createCar.ts
@@ -9,9 +9,9 @@ export default async function createCar(req: Request, res: Response) {
   try {
     // STEP 1: Validate the request body using the create car schema.
     const { body } = req;
+
     const validatedBody = await createCarSchema.safeParseAsync(body);
 
-    // If validation is unsuccessful return an error.
     if (!validatedBody.success) {
       return res.status(400).json({
         success: false,
@@ -19,10 +19,16 @@ export default async function createCar(req: Request, res: Response) {
       });
     }
 
-    // STEP 2: If validation is successful create and return the created car.
+    // STEP 2: If validation is successful create the car using the data created from validation.
     const createdCar = await prisma.car.create({
       data: validatedBody.data,
     });
+
+    // STEP 3: Return the created car if found otherwise return an error.
+    if (!createdCar) {
+      return res.status(400).json({ success: false });
+    }
+
     return res.status(201).json({ success: true, data: createdCar });
   } catch (error) {
     // If an error occurs then log the error and return an unsuccessful statement.

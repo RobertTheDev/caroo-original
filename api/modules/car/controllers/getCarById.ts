ce6c711c1f413537f7ed3b1513cfdd95d2aaaec7
RobertTheDev <robertthedev@gmail.com>
2023-10-02 02:13:20 +0100

    feat: added winston logger and update commenting

--- api/modules/car/controllers/getCarById.ts
+++ api/modules/car/controllers/getCarById.ts
@@ -1,29 +1,32 @@
 import prisma from 'api/lib/db/prisma';
+import logger from 'api/lib/logger/winstonLogger';
 import { Request, Response } from 'express';
 
 // This handler gets a car using its unique id in the database.
 
 export default async function getCarById(req: Request, res: Response) {
   try {
-    // Get id from the request params.
+    // STEP 1: Get car from the database using id from the request params.
     const { id } = req.params;
 
-    // Get car from the database.
     const car = await prisma.car.findUnique({
       where: {
         id,
       },
     });
 
-    // If no car was found return an error.
+    // STEP 2: Return the found car or an error if not found.
     if (!car) {
       return res.status(400).json({ success: false });
     }
 
-    // If a car was found return the car.
     return res.status(200).json({ success: true, data: car });
   } catch (error) {
-    // Return an error if one occurs.
+    // If an error occurs then log the error and return an unsuccessful statement.
+    const errorMessage = (error as Error).message;
+    logger.error(
+      `Error in route ${req.method} ${req.originalUrl}: ${errorMessage}`
+    );
     return res.status(400).json({ success: false });
   }
 }

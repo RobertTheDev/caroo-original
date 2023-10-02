7a20e9bfe86cec32ace7c1ec8b2979516a701518
RobertTheDev <robertthedev@gmail.com>
2023-10-02 01:21:38 +0100

    ci: exported page layout

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -4,16 +4,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 
-
-
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    PageLayoutComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent, PageLayoutComponent],
+  imports: [CommonModule],
+  exports: [PageLayoutComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}

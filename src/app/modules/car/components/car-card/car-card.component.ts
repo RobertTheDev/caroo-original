e8092fa229847500ceb3bb956a1df41de0d56b6d
RobertTheDev <robertthedev@gmail.com>
2023-10-02 01:41:57 +0100

    feat: defined car input

--- src/app/modules/car/components/car-card/car-card.component.ts
+++ src/app/modules/car/components/car-card/car-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import ICar from 'models/car/interfaces/Car';
 
 @Component({
   selector: 'app-car-card',
   templateUrl: './car-card.component.html',
-  styleUrls: ['./car-card.component.scss']
+  styleUrls: ['./car-card.component.scss'],
 })
 export class CarCardComponent {
-
+  @Input() car!: ICar;
 }
